0873e1c90bf4095628f17b742a0dd2e456b8dae4
Jacob Okulo <jacobodoyo56@gmail.com>
2026-06-22 12:42:16 +0300

    fix cors for vercel frontend

--- my-backend/server.js
+++ my-backend/server.js
@@ -54,7 +54,7 @@ app.use(
       "http://localhost:5173",
       "https://jacobodoyo.github.io",
       "https://jacobodoyo.github.io/Business_app",
-      //"https://business-kd766ajis-jacobodoyos-projects.vercel.app",
+      "https://business-app-pvy8-git-main-jacobodoyos-projects.vercel.app",
       //"https://business-app-lac.vercel.app",
     ],
     credentials: true,
